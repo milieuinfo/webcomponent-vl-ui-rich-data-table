fe6cf731ade110c5f2f55a81a8cb5dfa3d556ba4
Pieter Beckers <pieter.beckers@vlaanderen.be>
2019-11-28 11:56:53 +0100

    UIG-258: remove generieke search filter en voeg minder generieke demo toe; update deps

--- vl-rich-table.src.js
+++ vl-rich-table.src.js
@@ -2,15 +2,12 @@ import {define} from "/node_modules/vl-ui-core/vl-core.js";
 import {VlRichTable} from "./src/vl-rich-table";
 import {VlRichTableField} from "./src/vl-rich-table-field";
 import {VlRichTablePager} from "./src/vl-rich-table-pager";
-import {VlRichTableSearchFilter} from "./src/vl-rich-table-search-filter";
 
 define('vl-rich-table', VlRichTable);
 define('vl-rich-table-field', VlRichTableField);
 define('vl-rich-table-pager', VlRichTablePager);
-define('vl-rich-table-search-filter', VlRichTableSearchFilter);
 
 export * from "./src/vl-rich-table";
 export * from "./src/vl-rich-table-field";
 export * from "./src/vl-rich-table-pager";
-export * from "./src/vl-rich-table-search-filter";
 export * from "./src/domain/sortable";
