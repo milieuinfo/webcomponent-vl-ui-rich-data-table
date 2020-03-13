d02be2f3f5fab5c5871f3b093dbe2df2563ec29c
wautergu <guy.wauters@vlaanderen.be>
2020-03-13 14:29:14 +0100

    lege e2e testen groen maken

--- test/e2e/rich-data-table.test.js
+++ test/e2e/rich-data-table.test.js
@@ -2,10 +2,10 @@ const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VLRichDataTablePage = require('./pages/vl-rich-data-table.page');
 
 describe('vl-rich-data-table', async () => {
-    const VLRichDataTablePage = new VLRichDataTablePage(driver);
+    const vLRichDataTablePage = new VLRichDataTablePage(driver);
 
     before(() => {
-        return VLRichDataTablePage.load();
+        return vLRichDataTablePage.load();
     });
 
     it('', async () => {
