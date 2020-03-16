c267e655c555112d1333b38dae592dbfac27d191
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-03-16 09:27:14 +0100

    e2e test voor paginatie in rich-data-table

--- test/e2e/components/vl-rich-data-table.js
+++ test/e2e/components/vl-rich-data-table.js
@@ -1,10 +1,21 @@
 const { VlElement } = require('vl-ui-core').Test;
+const { By } = require('vl-ui-core').Test.Setup;
+const { VlPager } = require('vl-ui-pager').Test;
+const { VlDataTable } = require('vl-ui-data-table').Test;
 
-class VLRichDataTable extends VlElement {  
-    constructor(driver, selector) {
-        super(driver, selector);
-    }
+class VlRichDataTable extends VlElement {  
 
+	async getPager() {
+        const slot = await this.shadowRoot.findElement(By.css('slot[name="pager"]'));
+	    const assignedElements = await this.getAssignedElements(slot);
+	    return new VlPager(this.driver, assignedElements[0]);
+	}
+
+	async getDataTable() {
+		const dataTable = await this.shadowRoot;
+		return new VlDataTable(this.driver, dataTable);
+	}
+	
 }
 
-module.exports = VLRichDataTable;
+module.exports = VlRichDataTable;
