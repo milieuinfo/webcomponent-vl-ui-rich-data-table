86489997e368eb5b00a9360ab8f3f19b697aaa6e
Pieter Beckers <pieter.beckers@vlaanderen.be>
2020-03-26 14:42:16 +0100

    UIG-612: private component niet in commonJs export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = { 
     VlRichDataTable: require('../dist/vl-rich-data-table.src.js').VlRichDataTable,
-    VlRichDataField: require('../dist/vl-rich-data-field.src.js').VlRichDataField,
-    VlRichDataSorter: require('../dist/vl-rich-data-sorter.src.js').VlRichDataSorter
-}
+    VlRichDataField: require('../dist/vl-rich-data-field.src.js').VlRichDataField
+};
