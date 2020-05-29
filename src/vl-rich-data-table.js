2a4a70998d1c33c9140eead0bfe037d93c8e53fc
Pieter Beckers <pieter.beckers@vlaanderen.be>
2020-05-29 13:17:11 +0200

    UIG-679: fix paginatie bij waarde 0

--- src/vl-rich-data-table.js
+++ src/vl-rich-data-table.js
@@ -283,13 +283,13 @@ export class VlRichDataTable extends VlElement(HTMLElement) {
 
     set _paging(paging) {
         if (paging) {
-            if (paging.currentPage) {
+            if (paging.currentPage != null) {
                 this.__pager.setAttribute('current-page', paging.currentPage);
             }
-            if (paging.itemsPerPage) {
+            if (paging.itemsPerPage != null) {
                 this.__pager.setAttribute('items-per-page', paging.itemsPerPage);
             }
-            if (paging.totalItems) {
+            if (paging.totalItems != null) {
                 this.__pager.setAttribute('total-items', paging.totalItems);
             }
         }
