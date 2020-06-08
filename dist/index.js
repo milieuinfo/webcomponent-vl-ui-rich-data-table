20c61a1973ed50e83057dedaeb0d517f25b09b2d
Tom Coemans <tom.coemans@gmail.com>
2020-06-08 15:05:45 +0200

    dist remove cache

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  VlRichDataTable: require('../dist/vl-rich-data-table.src.js').VlRichDataTable,
-  VlRichDataField: require('../dist/vl-rich-data-field.src.js').VlRichDataField,
-};
