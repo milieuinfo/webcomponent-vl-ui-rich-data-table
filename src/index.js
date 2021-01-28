5dd95388f6034a3cf250a6e9c74fa14d928e0eca
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 18:30:14 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { VlRichDataTable } from '../dist/vl-data-table.src.js';
-import { VlRichDataField } from '../dist/vl-data-field.src.js';
-import { VlRichDataSorter } from '../dist/vl-data-sorter.src.js';
+import { VlRichDataTable } from '../dist/vl-rich-data-table.src.js';
+import { VlRichDataField } from '../dist/vl-rich-data-field.src.js';
+import { VlRichDataSorter } from '../dist/vl-rich-data-sorter.src.js';
 
 export {
     VlRichDataTable,
