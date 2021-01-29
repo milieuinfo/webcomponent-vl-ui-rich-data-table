cfa4d088c6ee8580c4cf5a05d8653e39633ce202
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 12:31:30 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import { VlRichDataTable } from '../dist/vl-rich-data-table.src.js';
-import { VlRichDataField } from '../dist/vl-rich-data-field.src.js';
-import { VlRichDataSorter } from '../dist/vl-rich-data-sorter.src.js';
-
-export {
-    VlRichDataTable,
-    VlRichDataField,
-    VlRichDataSorter
-};
+export * from '../dist/vl-rich-data-table-all.src.js';
