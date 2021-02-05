4fa5c4b5806165f0b742cc4ffe0a3b89fe683a2a
coemans <tom.coemans@vlaanderen.be>
2021-02-05 16:04:50 +0100

    introduced file with all imports (#89)

---
+++ dist/vl-rich-data-table-all.js
@@ -0,0 +1,9 @@
+import {VlRichDataField} from '/node_modules/vl-ui-rich-data-table/dist/vl-rich-data-field.js';
+import {VlRichDataSorter} from '/node_modules/vl-ui-rich-data-table/dist/vl-rich-data-sorter.js';
+import {VlRichDataTable} from '/node_modules/vl-ui-rich-data-table/dist/vl-rich-data-table.js';
+
+export {
+  VlRichDataField,
+  VlRichDataSorter,
+  VlRichDataTable,
+};
---
+++ dist/vl-rich-data-table-all.src.js
@@ -0,0 +1,10 @@
+import {VlRichDataField} from '../dist/vl-rich-data-field.src.js';
+import {VlRichDataSorter} from '../dist/vl-rich-data-sorter.src.js';
+import {VlRichDataTable} from '../dist/vl-rich-data-table.src.js';
+
+export {
+  VlRichDataField,
+  VlRichDataSorter,
+  VlRichDataTable,
+};
+
